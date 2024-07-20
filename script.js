8a2f119b62d52bcf363f486372d081a71bde315e
arpiithahr22 <hrarpitha4@gmail.com>
2024-07-20 15:15:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#element', {
+    strings: ["",'frontend developer..', ' learner..','techie..','CSE student..'],
+    typeSpeed: 70,
+  });
+  var typed = new Typed('#element2', {
+ strings: ['Eagerly waiting to expand my skills and knowledge in software fiild through hands on experience and opportunities... My curiosity drives me to experiment with new technologies... I beleive in lifelong learning and am committed to developing my skills and knowledge...'],
+ typeSpeed:80,
+  // Default value
+});
+
+function message(){
+  alert("Currently under process");
+}
